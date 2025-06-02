cfffaf4d4ce5f1ca346db71c1789bb88051933c9
Thaner <thanermerlo@gmail.com>
2025-06-02 09:24:22 +0000

    ReactByShinoda

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [contador, setContador] = useState(0);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const manejarClick = () => {
+    setContador(contador + 1);
+  };
+
+  return (
+    <div className="contenedor-contador">
+      <h1>Contador: {contador}</h1>
+      <button className="boton-contar" onClick={manejarClick}>
+        Contar
+      </button>
+    </div>
+  );
 };
 
 export default Home;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-//Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-// index.css'
-import '../styles/index.css'
+// Estilos personalizados
+import '../styles/index.css';
 
-// components
+// Componente principal
 import Home from './components/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+    <Home />
+  </React.StrictMode>
+);
